5f58d1ac1a50b603087a67664593f4e9aa5dc2e3
prasadsawant7 <prasadsawant7git@gmail.com>
2024-12-02 14:15:15 +0530

    initial commit

---
+++ lib/lib.cpp
@@ -0,0 +1,6 @@
+#include "lib.h"
+#include <iostream>
+
+void print_hello_world() {
+  std::cout << "Hello World!" << std::endl;
+}
---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "lib.h"
+#include <fmt/core.h>
+
+int main() {
+  print_hello_world();
+  fmt::print("Testing\b");
+}
